b53a080ce24144746adaa8c8a0bd4b1a881322ca
Andrew <reznichenko.a200212@gmail.com>
2025-08-12 18:20:28 +0300

    My try 5

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -4,14 +4,14 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class FileWork {
     public String[] readFromFile(String fileName) {
 
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             StringBuilder fileText = new StringBuilder();
             String value = bufferedReader.readLine();
             while (value != null) {
@@ -21,10 +21,13 @@ public class FileWork {
             String[] arrTexts = fileText.toString().toLowerCase().split("\\W+");
             ArrayList<String> text = new ArrayList<>();
             for (String arrText : arrTexts) {
-                if (arrText.startsWith("w")) {
-                    text.add(arrText);
+                if (!arrText.isEmpty() && arrText.startsWith("w")) {
+                    if (arrText.startsWith("w")) {
+                        text.add(arrText);
+                    }
                 }
             }
+            Collections.sort(text);
             return text.toArray(new String[0]);
         } catch (FileNotFoundException e) {
             throw new RuntimeException("Can`t found the file", e);
